e63f48a2e68add0309ea2f37bacfa3614ba1a2d9
islam soliman <islam.soliman1990@gmail.com>
2023-06-10 07:04:41 +0300

    auth login function

--- index.jsx
+++ index.jsx
@@ -1,115 +1,107 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import {
   RouterProvider,
   createBrowserRouter,
   createRoutesFromElements,
   Route,
-  Link,
-  redirect,
-} from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Vans, { loader as vansLoader } from "./pages/Vans/Vans";
-import VanDetail, { loader as vanDetailLoader } from "./pages/Vans/VanDetail";
-import Dashboard from "./pages/Host/Dashboard";
-import Income from "./pages/Host/Income";
-import Reviews from "./pages/Host/Reviews";
-import HostVans, { loader as hostVansLoader } from "./pages/Host/HostVans";
-import HostVanDetail, {
-  loader as hostVanDetailLoader,
-} from "./pages/Host/HostVanDetail";
-import HostVanInfo from "./pages/Host/HostVanInfo";
-import HostVanPricing from "./pages/Host/HostVanPricing";
-import HostVanPhotos from "./pages/Host/HostVanPhotos";
-import NotFound from "./pages/NotFound";
-import Login from "./pages/Login";
-import Layout from "./components/Layout";
-import HostLayout from "./components/HostLayout";
-import Error from "./components/Error";
+  Link
+} from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Vans, { loader as vansLoader } from "./pages/Vans/Vans"
+import VanDetail, { loader as vanDetailLoader } from "./pages/Vans/VanDetail"
+import Dashboard from "./pages/Host/Dashboard"
+import Income from "./pages/Host/Income"
+import Reviews from "./pages/Host/Reviews"
+import HostVans, { loader as hostVansLoader} from "./pages/Host/HostVans"
+import HostVanDetail, { loader as hostVanDetailLoader } from "./pages/Host/HostVanDetail"
+import HostVanInfo from "./pages/Host/HostVanInfo"
+import HostVanPricing from "./pages/Host/HostVanPricing"
+import HostVanPhotos from "./pages/Host/HostVanPhotos"
+import NotFound from "./pages/NotFound"
+import Login, { loader as loginLoader } from "./pages/Login"
+import Layout from "./components/Layout"
+import HostLayout from "./components/HostLayout"
+import Error from "./components/Error"
+import { requireAuth } from "./utils"
 
-import "./server";
-let auth = false;
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Layout />}>
-      <Route index element={<Home />} />
-      <Route path="about" element={<About />} />
-      <Route path="login" element={<Login />} />
+import "./server"
+
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path="/" element={<Layout />}>
+    <Route index element={<Home />} />
+    <Route path="about" element={<About />} />
+    <Route
+      path="login"
+      element={<Login />}
+      loader={loginLoader}
+    />
+    <Route
+      path="vans"
+      element={<Vans />}
+      errorElement={<Error />}
+      loader={vansLoader}
+    />
+    <Route 
+      path="vans/:id" 
+      element={<VanDetail />} 
+      loader={vanDetailLoader}
+    />
+
+    <Route path="host" element={<HostLayout />}>
+      <Route
+        index
+        element={<Dashboard />}
+        loader={async () => await requireAuth()}
+      />
+      <Route
+        path="income"
+        element={<Income />}
+        loader={async () => await requireAuth()}
+      />
+      <Route
+        path="reviews"
+        element={<Reviews />}
+        loader={async () => await requireAuth()}
+      />
       <Route
         path="vans"
-        element={<Vans />}
-        errorElement={<Error />}
-        loader={vansLoader}
+        element={<HostVans />}
+        loader={hostVansLoader}
       />
-      <Route path="vans/:id" element={<VanDetail />} loader={vanDetailLoader} />
-      <Route path="host" element={<HostLayout />}>
+      <Route
+        path="vans/:id"
+        element={<HostVanDetail />}
+        loader={hostVanDetailLoader}
+      >
         <Route
           index
-          element={<Dashboard />}
-          loader={async () => {
-            if (!auth) {
-              throw redirect("/login");
-            }
-            return null;
-          }}
+          element={<HostVanInfo />}
+          loader={async () => await requireAuth()}
         />
         <Route
-          path="income"
-          element={<Income />}
-          loader={async () => {
-            if (!auth) {
-              throw redirect("/login");
-            }
-            return null;
-          }}
+          path="pricing"
+          element={<HostVanPricing />}
+          loader={async () => await requireAuth()}
         />
         <Route
-          path="reviews"
-          element={<Reviews />}
-          loader={async () => {
-            if (!auth) {
-              throw redirect("/login");
-            }
-            return null;
-          }}
+          path="photos"
+          element={<HostVanPhotos />}
+          loader={async () => await requireAuth()}
         />
-        <Route path="vans" element={<HostVans />} loader={hostVansLoader} />
-        <Route
-          path="vans/:id"
-          element={<HostVanDetail />}
-          loader={hostVanDetailLoader}
-        >
-          <Route
-            index
-            element={<HostVanInfo />}
-            loader={async () => {
-              return null;
-            }}
-          />
-          <Route
-            path="pricing"
-            element={<HostVanPricing />}
-            loader={async () => {
-              return null;
-            }}
-          />
-          <Route
-            path="photos"
-            element={<HostVanPhotos />}
-            loader={async () => {
-              return null;
-            }}
-          />
-        </Route>
       </Route>
-      <Route path="*" element={<NotFound />} />
     </Route>
-  )
-);
+    <Route path="*" element={<NotFound />} />
+  </Route>
+))
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <RouterProvider router={router} />
+  )
 }
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM
+  .createRoot(document.getElementById('root'))
+  .render(<App />);
--- pages/Login.jsx
+++ pages/Login.jsx
@@ -1,43 +1,69 @@
-import React from "react"
-import { useNavigate } from "react-router-dom"
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+import { loginUser } from "../api";
 
+export function loader({ request }) {
+  return new URL(request.url).searchParams.get("message");
+}
+
+/**
+ * Challenge: hook up our form so it (halfway) works.
+ *
+ * 1. Pull in the `loginUser` function from the api.js file
+ * 2. Call loginUser when the form is submitted and console.log
+ *    the data that comes back. Use "b@b.com" as the username and
+ *    "p123" as the password.
+ *
+ *    NOTE: loginUser returns a promise, so you'll need
+ *    a .then(data => {...}) to access the data, or use
+ *    a separate aync function defined inside handleSubmit
+ * 3. TBA
+ */
+export function Loader({ request }) {
+  return new URL(request.url).searchParams.get("message");
+}
 export default function Login() {
-    const [loginFormData, setLoginFormData] = React.useState({ email: "", password: "" })
+  const [loginFormData, setLoginFormData] = React.useState({
+    email: "",
+    password: "",
+  });
+  const message = useLoaderData();
 
-    function handleSubmit(e) {
-        e.preventDefault()
-        console.log(loginFormData)
-    }
+  async function handleSubmit(e) {
+    e.preventDefault();
+    let userData = await loginUser(loginFormData);
+    console.log(userData);
+  }
 
-    function handleChange(e) {
-        const { name, value } = e.target
-        setLoginFormData(prev => ({
-            ...prev,
-            [name]: value
-        }))
-    }
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setLoginFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  }
 
-    return (
-        <div className="login-container">
-            <h1>Sign in to your account</h1>
-            <form onSubmit={handleSubmit} className="login-form">
-                <input
-                    name="email"
-                    onChange={handleChange}
-                    type="email"
-                    placeholder="Email address"
-                    value={loginFormData.email}
-                />
-                <input
-                    name="password"
-                    onChange={handleChange}
-                    type="password"
-                    placeholder="Password"
-                    value={loginFormData.password}
-                />
-                <button>Log in</button>
-            </form>
-        </div>
-    )
-
+  return (
+    <div className="login-container">
+      <h1>Sign in to your account</h1>
+      {message && <h3 className="red">{message}</h3>}
+      <form onSubmit={handleSubmit} className="login-form">
+        <input
+          name="email"
+          onChange={handleChange}
+          type="email"
+          placeholder="Email address"
+          value={loginFormData.email}
+        />
+        <input
+          name="password"
+          onChange={handleChange}
+          type="password"
+          placeholder="Password"
+          value={loginFormData.password}
+        />
+        <button>Log in</button>
+      </form>
+    </div>
+  );
 }
