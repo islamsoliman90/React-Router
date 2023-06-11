fc215eafe57caa644203d6c7472f219169264004
islam soliman <islam.soliman1990@gmail.com>
2023-06-11 21:46:31 +0300

    useNavigate

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -1,40 +1,49 @@
-import React from "react";
-import { useLoaderData } from "react-router-dom";
+import React, { useState } from "react";
+import { useLoaderData, useNavigate } from "react-router-dom";
 import { loginUser } from "../api";
 
 export function loader({ request }) {
-  return new URL(request.url).searchParams.get("message");
+  // return new URL(request.url).searchParams.get("message");
+  return null;
 }
 
 /**
- * Challenge: hook up our form so it (halfway) works.
- *
- * 1. Pull in the `loginUser` function from the api.js file
- * 2. Call loginUser when the form is submitted and console.log
- *    the data that comes back. Use "b@b.com" as the username and
- *    "p123" as the password.
- *
- *    NOTE: loginUser returns a promise, so you'll need
- *    a .then(data => {...}) to access the data, or use
- *    a separate aync function defined inside handleSubmit
- * 3. TBA
+ * Challenge: Code the sad path 🙁
+ * 3. Add a `status` state and default it to "idle". When the
+ *    login form begins submitting, set it to "submitting". When
+ *    it's done submitting (whether successful or not), set it
+ *    to "idle" again.
+ * 4. Disable the button when the `status` state is "submitting"
+ *    (this may require some Googling if you haven't done this
+ *    before).
+ * 5. Add an `error` state and default it to `null`. When the
+ *    form is submitted, reset the errors to `null`. If there's
+ *    an error from `loginUser` (add a .catch(err => {...}) in
+ *    the promise chain), set the error to the error that
+ *    comes back.
+ * 6. Display the error.message below the `h1` if there's ever
+ *    an error in state
  */
-export function Loader({ request }) {
-  return new URL(request.url).searchParams.get("message");
-}
+
 export default function Login() {
+  const [state, setState] = useState("idle");
+  const [error, setError] = useState(null);
   const [loginFormData, setLoginFormData] = React.useState({
     email: "",
     password: "",
   });
   const message = useLoaderData();
-
-  async function handleSubmit(e) {
+  const navigate = useNavigate();
+  function handleSubmit(e) {
     e.preventDefault();
-    let userData = await loginUser(loginFormData);
-    console.log(userData);
+    setState(() => "submitting");
+    setError(null);
+    loginUser(loginFormData)
+      .then((data) => navigate("/host"))
+      .catch((e) => setError(() => e))
+      .finally(() => setState("idle"));
   }
-
+  console.log(message, state);
   function handleChange(e) {
     const { name, value } = e.target;
     setLoginFormData((prev) => ({
@@ -47,6 +56,7 @@ export default function Login() {
     <div className="login-container">
       <h1>Sign in to your account</h1>
       {message && <h3 className="red">{message}</h3>}
+      {error && <h3 className="red">{error.message}</h3>}
       <form onSubmit={handleSubmit} className="login-form">
         <input
           name="email"
@@ -62,7 +72,9 @@ export default function Login() {
           placeholder="Password"
           value={loginFormData.password}
         />
-        <button>Log in</button>
+        <button disabled={state !== "idle" ? true : false}>
+          {state != "idle" ? "Loading" : " Log in"}
+        </button>
       </form>
     </div>
   );
