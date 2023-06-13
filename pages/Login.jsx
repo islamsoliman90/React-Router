2fb76777f93373287b8d5583db88053d9c13d3d5
islam soliman <islam.soliman1990@gmail.com>
2023-06-13 07:32:16 +0300

    sad path action Form

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -1,5 +1,12 @@
 import React from "react";
-import { useLoaderData, useNavigate, redirect, Form } from "react-router-dom";
+import {
+  useLoaderData,
+  useNavigate,
+  Form,
+  redirect,
+  useActionData,
+  useNavigation,
+} from "react-router-dom";
 import { loginUser } from "../api";
 
 export function loader({ request }) {
@@ -7,58 +14,33 @@ export function loader({ request }) {
 }
 
 export async function action({ request }) {
-  console.log("Action function");
-  let formData = await request.formData();
-  let email = formData.get("email");
-  let password = formData.get("password");
-  let data = await loginUser({ email, password });
-  localStorage.setItem("isLoginUser", true);
-  throw redirect("/host");
-
-  return null;
+  const formData = await request.formData();
+  const email = formData.get("email");
+  const password = formData.get("password");
+  try {
+    const data = await loginUser({ email, password });
+    localStorage.setItem("loggedin", true);
+    return redirect("/host");
+  } catch (err) {
+    return err.message;
+  }
 }
 
 export default function Login() {
-  const [loginFormData, setLoginFormData] = React.useState({
-    email: "",
-    password: "",
-  });
-  const [status, setStatus] = React.useState("idle");
-  const [error, setError] = React.useState(null);
+  let navigation = useNavigation();
   const message = useLoaderData();
   const navigate = useNavigate();
-
-  //   function handleSubmit(e) {
-  //     e.preventDefault();
-  //     setStatus("submitting");
-  //     setError(null);
-  //     loginUser(loginFormData)
-  //       .then((data) => {
-  //         navigate("/host", { replace: true });
-  //       })
-  //       .catch((err) => setError(err))
-  //       .finally(() => setStatus("idle"));
-  //   }
-
-  //   function handleChange(e) {
-  //     const { name, value } = e.target;
-  //     setLoginFormData((prev) => ({
-  //       ...prev,
-  //       [name]: value,
-  //     }));
-  //   }
-
+  let err = useActionData();
   return (
     <div className="login-container">
       <h1>Sign in to your account</h1>
       {message && <h3 className="red">{message}</h3>}
-      {error && <h3 className="red">{error.message}</h3>}
-
+      {err && <h3 className="red">{err}</h3>}
       <Form method="post" className="login-form" replace>
         <input name="email" type="email" placeholder="Email address" />
         <input name="password" type="password" placeholder="Password" />
-        <button disabled={status === "submitting"}>
-          {status === "submitting" ? "Logging in..." : "Log in"}
+        <button disabled={navigation.state === "submitting"}>
+          {navigation.state === "submitting" ? "Logging in..." : "Log in"}
         </button>
       </Form>
     </div>
