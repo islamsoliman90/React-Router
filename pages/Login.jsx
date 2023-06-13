6a4c944bdb816225766d012610f8f55b4ad638b6
islam soliman <islam.soliman1990@gmail.com>
2023-06-13 18:17:28 +0300

    redirect to same place

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import {
   useLoaderData,
-  useNavigate,
+  useNavigation,
   Form,
   redirect,
   useActionData,
-  useNavigation,
 } from "react-router-dom";
 import { loginUser } from "../api";
 
@@ -17,25 +16,27 @@ export async function action({ request }) {
   const formData = await request.formData();
   const email = formData.get("email");
   const password = formData.get("password");
+  const path = new URL(request.url).searchParams.get("url");
   try {
     const data = await loginUser({ email, password });
     localStorage.setItem("loggedin", true);
-    return redirect("/host");
+    return redirect(`../${path}`);
   } catch (err) {
     return err.message;
   }
 }
 
 export default function Login() {
-  let navigation = useNavigation();
+  const errorMessage = useActionData();
   const message = useLoaderData();
-  const navigate = useNavigate();
-  let err = useActionData();
+  const navigation = useNavigation();
+
   return (
     <div className="login-container">
       <h1>Sign in to your account</h1>
       {message && <h3 className="red">{message}</h3>}
-      {err && <h3 className="red">{err}</h3>}
+      {errorMessage && <h3 className="red">{errorMessage}</h3>}
+
       <Form method="post" className="login-form" replace>
         <input name="email" type="email" placeholder="Email address" />
         <input name="password" type="password" placeholder="Password" />
--- utils.js
+++ utils.js
@@ -1,9 +1,9 @@
-import { redirect } from "react-router-dom"
+import { redirect } from "react-router-dom";
 
-export async function requireAuth() {
-    const isLoggedIn = localStorage.getItem("loggedin")
-    
-    if (!isLoggedIn) {
-        throw redirect("/login?message=You must log in first.")
-    }
+export async function requireAuth(request) {
+  const isLoggedIn = localStorage.getItem("loggedin");
+  const redirectTo = new URL(request.url).pathname || "/";
+  if (!isLoggedIn) {
+    throw redirect(`/login?message=You must log in first.&&url=${redirectTo}`);
+  }
 }
